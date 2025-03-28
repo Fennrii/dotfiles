15dda0ea8ce4cf3db20d17dff755f23fe4e063f0
Andrew Satory <axsateagle@gmail.com>
2025-03-28 00:59:58 -0400

    colour update

--- updaters/nvim.sh
+++ updaters/nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y nodejs npm
+sudo apt install -y nodejs npm xclip
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
